2e46ae7efa01f091015a920252bb94b2cb8954c9
alankillip <alan.kilip@dentsuaegis.com>
2019-08-31 17:12:03 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const port = process.env.PORT || 3001;
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
